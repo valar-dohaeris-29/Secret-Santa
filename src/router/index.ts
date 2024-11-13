8fbdf7f094683be7dbd511e132df6ae3ee8b3090
Deuel <deuel.ellan@iemas.co.za>
2024-11-13 22:40:37 +0200

    remove lazy loading

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Ellans from "../views/santa-circles/Ellans.vue";
+import Cathy from "@/views/santa-circles/Cathy.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,10 +13,11 @@ const router = createRouter({
     {
       path: "/cathy",
       name: "cathy",
+      component: Cathy,
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/santa-circles/Cathy.vue"),
+      // component: () => import("../views/santa-circles/Cathy.vue"),
     },
   ],
 });
