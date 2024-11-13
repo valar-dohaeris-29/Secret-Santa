6a4b73b20ebc4ba8d2a5591443562d5a10d14311
Deuel <deuel.ellan@iemas.co.za>
2024-11-13 22:54:27 +0200

    create Web Hash History

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory  } from "vue-router";
 import Ellans from "../views/santa-circles/Ellans.vue";
 import Cathy from "@/views/santa-circles/Cathy.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
